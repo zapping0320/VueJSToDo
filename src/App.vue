86cecf064b125b2070eb934d041d472a0f1293aa
john donghyun kim <zapping0320@gmail.com>
2021-01-20 16:11:34 +0900

    Root, TodoHeader style 추가

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
 <template>
   <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoList></TodoList>
+    <TodoInput></TodoInput>
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
 <script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoList   from './components/TodoList.vue'
+import TodoInput  from './components/TodoInput.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
 export default {
+  components: {
+    'TodoHeader'  : TodoHeader,
+    'TodoList'    : TodoList,
+    'TodoInput'   : TodoInput,
+    'TodoFooter'  : TodoFooter,
+
+  }
 }
+
+
 </script>
 
 <style>
+  body {
+        text-align: center;
+        background-color: #f6f6f8;
+    }
+
+    input {
+        border-style: groove;
+        width: 200px;
+    }
+
+    button {
+        border-style: groove;
+    }
+
+    .shadow {
+        box-shadow: 5px 10px 10px rgba(0,0,0,0.03)
+    }
 </style>
