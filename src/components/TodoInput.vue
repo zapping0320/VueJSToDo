d3474dca0d76630950dd775d28d214e319e5459e
john donghyun kim <zapping0320@gmail.com>
2021-01-20 16:38:54 +0900

    TodoInput 기본 기능 구현

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,10 +1,26 @@
 <template>
-    <div>input</div>
+    <div>
+        <input type="text" v-model="newTodoItem">
+        <button v-on:click="addTodo">add </button>
+
+    </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            newTodoItem:''   
+        }
+    },
+
+    methods: {
+        addTodo() {
+            console.log(this.newTodoItem);
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+        }
+
+    }
 }
 </script>
 
