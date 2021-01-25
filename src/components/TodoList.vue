e5691a95ccf112a58d1e7b0c954139fb9cfb5e63
john donghyun kim <zapping0320@gmail.com>
2021-01-26 08:44:59 +0900

    누락 문법 추가 - :key

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
     <section>
         <ul>
-            <li v-for="(todoItem, index) in todoItems" class="shadow">
+            <li v-for="(todoItem, index) in todoItems" :key="todoItem" class="shadow">
                 <i class="checkBtn fas fa-check" aria-hidden="true"></i>
                 {{ todoItem }}
                 <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
