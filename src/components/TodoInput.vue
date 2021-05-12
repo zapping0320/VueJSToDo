b87ea0e379b02be0960eb2ec77ec7c46b0336414
john donghyun kim <zapping0320@gmail.com>
2021-05-12 10:35:33 +0900

    todoinput에 modal component 등록

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,16 +4,26 @@
         <span class="addContainer" v-on:click="addTodo">
             <i class="addBtn fas fa-plus" arial-hidden="true"></i>
         </span>
-        
+        <!-- use the modal component, pass in the prop -->
+      <Modal v-if="showModal" @close="showModal = false">
+        <!--
+      you can use custom content here to overwrite
+      default content
+    -->
+        <h3 slot="header">custom header</h3>
+      </Modal>
 
     </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
+
 export default {
     data() {
         return {
-            newTodoItem:''   
+            newTodoItem:'',
+            showModal: false
         }
     },
 
@@ -29,8 +39,10 @@ export default {
         clearInput() {
                 this.newTodoItem = '';
         }
-        
+    },
 
+    components: {
+        Modal: Modal
     }
 }
 </script>
